32cdf3c3d2a1924b9661259fc4b5888d301969d4
unknown <Digital@DH-A10-17.dhbrasil.digitalhouse.com>
2020-01-15 12:06:43 -0300

    Javascript To Do

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let board  = document.getElementById('board');
+let buttonAdd = document.getElementById('add');
+let inputAdd = document.getElementById('novaTarefa');
+let listaTarefas = [];
+
+if(localStorage.getItem('listaTarefas')){
+    listaTarefas = JSON.parse(localStorage.getItem('listaTarefas'))
+}else {
+ listaTarefas = [];
+    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+}
+
+mostrarNaTela(listaTarefas);
+
+
+buttonAdd.onclick = function(){
+    let valorDigitado = inputAdd.value;
+    listaTarefas.push(valorDigitado);
+
+    let tarefa = document.createElement('div');
+    //<div>
+    tarefa.setAttribute('class','tarefa');
+
+    let titulo = document.createElement('div');
+    titulo.setAttribute('class', 'col-md-8');
+    titulo.textContent = valorDigitado;
+
+    let buttonCheck = document.createElement('div');
+    buttonCheck.setAttribute('class', 'col-md-4');
+
+    let button = document.createElement('button');
+    buttonCheck.setAttribute('class','btn btn-success')
+    buttonCheck.textContent = "Check";
+    
+    tarefa.appendChild(titulo);
+    tarefa.appendChild(buttonCheck);
+
+    board.appendChild(tarefa);
+    
+    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));    
+}
+
+function mostrarNaTela(listaTarefas){
+    for(let item of listaTarefas){
+        gerarTarefa(item);
+    }
+}
+
+function gerarTarefa(valorDigitado){
+    let tarefa = document.createElement('div');
+    tarefa.setAttribute('class','tarefa');
+
+    let titulo = document.createElement('div');
+    titulo.setAttribute('class', 'col-md-8');
+    titulo.textContent = valorDigitado;
+
+    let buttonCheck = document.createElement('div');
+    buttonCheck.setAttribute('class', 'col-md-4');
+
+    let button = document.createElement('button');
+    buttonCheck.setAttribute('class','btn btn-success')
+    buttonCheck.textContent = "Check";
+    
+    tarefa.appendChild(titulo);
+    tarefa.appendChild(buttonCheck);
+
+    board.appendChild(tarefa);
+}
